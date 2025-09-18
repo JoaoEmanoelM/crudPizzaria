e4a89f724ee16e5cd3a6ac75cb4b276c56930c9c
João Emanoel Mendonça Matos <141441658+JoaoEmanoelM@users.noreply.github.com>
2025-09-18 17:38:23 -0300

    Delete crud-pizzaria directory

--- crud-pizzaria/view/include/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>&copy; 2025 Pizzaria Vovô Alberto. Todos os direitos reservados.</p>
-</footer>
--- crud-pizzaria/view/include/header.php
+++
@@ -1,4 +0,0 @@
-<header>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTP1a_zdlU8JNJDD7j8kNV_a1nIJsWX_TX8w&s" alt="Pizzaria Vovô Alberto Logo">
-    <h1>Pizzaria Vovô Alberto</h1>
-</header>
